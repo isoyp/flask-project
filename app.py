24d7889620cf208c5a27159cfd37ddf08d6810d4
dany <sarkazsumino@gmail.com>
2026-01-15 08:29:06 +0700

    triger redeploy

--- app.py
+++ app.py
@@ -1,22 +1,12 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def  home():
-    print("kontol")
-    return render_template("index.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
+from flask import Flask
 import os
 
+app = Flask(_name_)
 
-if __name__== "__main__" :
-    port = int(os.environ.get("port", 5000))
+@app.route("/")
+def index():
+    return "FLASK TEST OK v2"
+
+if _name_ == "_main_":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
-
-# triger redeploy
