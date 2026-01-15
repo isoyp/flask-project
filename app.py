7fda93d4bfe14f80646f04bc01562b6b8ec3cff5
dany <sarkazsumino@gmail.com>
2026-01-15 07:14:18 +0700

    update app, html, and requirements

--- app.py
+++ app.py
@@ -11,7 +11,11 @@ def  home():
 def about():
     return render_template("about.html")
 
+import os
+
+
 if __name__== "__main__" :
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("port", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
